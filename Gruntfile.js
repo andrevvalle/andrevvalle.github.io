140a138bef8f7e667756dc17621349e251f64eb7
André Valle <andrevvalle@icloud.com>
2014-10-26 16:16:40 -0200

    ajuste gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,5 @@
 module.exports = function(grunt){
 	grunt.initConfig({
-		uglify:{
-			my_target:{
-				files:{
-					'js/main.min.js' : ['js/main.js']
-				}	
-			}
-		},
-		cssmin: {
-		  combine: {
-		    files: {
-		      'css/main.min.css': ['css/main.css'],
-		      'css/animate.min.css': ['css/animate.css']
-		    }
-		  }
-		},
 		less: {
 		  development: {
 		    files: {
@@ -22,32 +7,75 @@ module.exports = function(grunt){
 		    }
 		  }
 		},
+		cssmin: {
+		  combine: {
+		    files: {
+		      'css/main.min.css': ['css/main.css']
+		    }
+		  }
+		},
+		jshint: {
+	    	all: ['Gruntfile.js', 'js/main.js']
+	  	},
+	  	uglify:{
+			my_target:{
+				files:{
+					'js/main.min.js' : ['js/main.js']
+				}	
+			}
+		},
+		connect :  { 
+	      server : {
+	        options : {
+	          hostname : 'localhost',
+	          port: 9000,
+	          keepalive: true,
+	          open: true,
+	        }
+	      }
+	    },
 		watch: {
 			gruntfile: {
                 files: ['Gruntfile.js']
             },
-			src: {
-		      files: ['js/*.js', 'css/*.less', 'css/*.css'],
-		      tasks: ['default'],
-		    },
-		},
-		connect: {
-		  'static': {
-		      options: {
-		          hostname: 'localhost',
-		          port: 9000
-		      }
-		  }
+            less: {
+            	files: ['css/*.less'],
+            	tasks: ['less']
+            },
+            cssmin: {
+            	files: ['css/main.css'],
+            	tasks: ['cssmin'],
+            	options: {
+            		livereload: true
+            	}
+            },
+            jshint: {
+            	files: ['js/main.js'],
+            	tasks: ['jshint']
+            },
+            uglify: {
+            	files: ['js/main.js'],
+            	tasks: ['uglify'],
+            	options: {
+            		livereload: true
+            	}
+            },
+            html: {
+            	files: ['*.html'],
+            	options: {
+            		livereload: true
+            	}
+            }
 		}
-
-	})
-
-
-	grunt.registerTask("default", ["uglify", "less", "cssmin", "watch"]);
+	});
+	
+	grunt.registerTask("watch", ["watch"]);
+	grunt.registerTask('server', ['connect:server']);
 
 	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks("grunt-contrib-cssmin");
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-}
+};
--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-$(document).ready( function(){
-	$( "#circle" ).mouseover(function() {
+$(document).ready(function() {
+	
+	$("#circle").mouseenter(function() {
 		$(this).addClass('animated flip');
-	});
-	$( "#circle" ).mouseout(function() {
 		setTimeout(function() {
 			$('#circle').removeClass('animated flip');
 		}, 1000);
+	});
+	$("#circle").mouseleave(function() {
+		clearTimeout(1000);
 	});
 });
